9deb8f5f5730e7726c4a5d433ec8d33ee18adf57
Raymond Ezra <134621588+Ray-Ezra@users.noreply.github.com>
2025-08-27 22:52:48 +0300

    Updates: I have increased the functionaluty capabilities by add more functions like makeMove(This defines the moves that are made by the player) and playerTurn this will allow players to interact by determining where the players move are(positions)

--- Tic_Tac_Toe/Main.hs
+++ Tic_Tac_Toe/Main.hs
@@ -6,12 +6,14 @@ main :: IO ()
 main = do
     putStrLn "Tic Tac Toe"
     displayBoard emptyBoard
+    newBoard <- playerTurn emptyBoard 'X'
+    displayBoard newBoard
 
 type Board = [Char]
 
 emptyBoard :: Board
 {-- 
-* Here I create a variable @emptyBoard@ which is a list of 9 spaces which uses the Replicate function, representing an empty Tic Tac Toe board.
+* Here I create a var @emptyBoard@ which is a list of 9 spaces which uses the Replicate function, representing an empty Tic Tac Toe board.
 * I moved on to create a function @displayBoard@ which takes a board as input and prints it in a 3x3 grid format.
 --}
 emptyBoard = replicate 9 ' '
@@ -22,3 +24,37 @@ displayBoard [a,b,c,d,e,f,g,h,i] = do
     putStrLn $ [d] ++  " | " ++ [e] ++ " | " ++ [f]
     putStrLn "---------"
     putStrLn $ [g] ++  " | " ++ [h] ++ " | " ++ [i]
+{--
+* Next, I created a type signature @makeMove@ which takes:
+    - a Board (list of 9 characters(a - i))
+    - Int the position (1-9)
+    - Char the player ('X' or 'O')
+    - returns a new Board with the player's move applied.
+*Then Created a function @makeMove@ which has the following parameters:
+    - board: the current state of the board
+    - pos: the position (1-9) where the player wants to place their mark
+    - player: the character ('X' or 'O') representing the player
+--}
+makeMove :: Board -> Int -> Char -> Board
+makeMove board pos player =
+    take (pos - 1) board ++ [player] ++ drop pos board
+{--
+* Next, Ive created another type signature @playerTurn@ the Board, the Char input and returns an IO Board.
+* Then I created a function @playerTurn@ which prompts the player for their move, checks if the move is valid, and updates the board accordingly. If the move is invalid, it prompts the player to try again.
+    The Two Functions
+    1. hFLush and stdout this is to ensure the prompt is displayed before waiting for the input of the player
+--}
+playerTurn :: Board -> Char -> IO Board
+playerTurn board player = do 
+    putStrLn $ "Player " ++ [player] ++ ", enter your move (1-9): "
+    hFlush stdout
+    input <- getLine
+    let pos = read input :: Int
+    if board !! (pos - 1) == ' '
+        then return (makeMove board pos player)
+        else do
+            putStrLn "Invalid move, try again."
+            playerTurn board player
+{--
+*
+--}
