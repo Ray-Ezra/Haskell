35d9f7ac8402234c489b348d946c9e4e3cb94884
Raymond Ezra <134621588+Ray-Ezra@users.noreply.github.com>
2025-08-29 11:14:32 +0300

    Made updates for task manager

--- Task_Manager/Main.hs
+++ Task_Manager/Main.hs
@@ -13,3 +13,32 @@ title = unlines
 main :: IO ()
 main = do
     putStrLn title
+{--
+* Here im defininig thet Task structure, This will hold the information about each task
+* The object has four properties: id, title, description, and completed and since they are related to task ive used the data keyword to define a new data type called Task
+--}
+data Task = Task {
+    taskId :: Int,
+    taskTitle :: String,
+    taskDescription :: String,
+    taskCompleted :: Bool
+} 
+{--
+* Creating Tasks
+--}
+createTask :: [Task] -> IO [Task]
+createTask task = do
+    title <- prompt "Enter task title: "
+    description <- prompt "Enter task description: "
+    let newTask = (nextId) title description False
+    putStrLn $ "Task created with ID: " ++ show (taskId newTask)
+    return (task ++ [newTask])
+{--
+* Read the Tasks
+--}
+{--
+* Update The Tasks
+--}
+{--
+* Delete Tasks
+--}
